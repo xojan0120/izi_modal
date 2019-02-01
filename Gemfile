e4330bc714f9cbce2b6bda647c45429c32a79364
xojan0120 <xojan0120@gmail.com>
2019-02-02 07:55:52 +0900

    [Update] アプリ名変更

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ group :development, :test do
   gem 'hirb'
   gem 'hirb-unicode'
 end
+gem 'rename'
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module IziModal
+module IziModalTest
   class Application < Rails::Application
     config.generators.fixture_replacement :factory_bot, dir: 'spec/factories'
     config.generators do |g| g.test_framework :rspec, view_specs: false, helper_specs: false, routing_specs: false, controller_specs: false end
